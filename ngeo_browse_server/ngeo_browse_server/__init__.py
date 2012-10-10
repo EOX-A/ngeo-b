573f999ac448b8b4e70ac8692f15ee91f065f1b1
Stephan Meissl <stephan@meissl.name>
2012-10-10 16:53:28 +0200

    Adjusting get_version().

---
+++ ngeo_browse_server/ngeo_browse_server/__init__.py
@@ -0,0 +1,48 @@
+#-------------------------------------------------------------------------------
+#
+# Project: ngEO Browse Server <http://ngeo.eox.at>
+# Authors: Fabian Schindler <fabian.schindler@eox.at>
+#          Marko Locher <marko.locher@eox.at>
+#          Stephan Meissl <stephan.meissl@eox.at>
+#
+#-------------------------------------------------------------------------------
+# Copyright (C) 2012 EOX IT Services GmbH
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+# copies of the Software, and to permit persons to whom the Software is 
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies of this Software or works derived from this Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#-------------------------------------------------------------------------------
+
+import re
+
+VERSION = (0, 1, None, 'dev', 0)
+#VERSION = (0, 1, None, 'alpha', 1)
+#VERSION = (0, 1, None, 'beta', 1)
+#VERSION = (0, 1, None, 'rc', 1)
+#VERSION = (0, 1, 0, 'final', 0)
+
+def get_version():
+    version = '%s.%s' % (VERSION[0], VERSION[1])
+    if VERSION[2] != None:
+        version = '%s.%s' % (version, VERSION[2])
+    if VERSION[3:] == ('dev', 0):
+        version = "%s%s" % (version, VERSION[3])
+    else:
+        if VERSION[3] != 'final':
+            version = '%s%s%s' % (version, VERSION[3], VERSION[4])
+    
+    return version
