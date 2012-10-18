8734f62eb581dfbd7db51483890e2a0bd904c030
Fabian Schindler <fabian.schindler@gmx.at>
2012-10-18 18:08:48 +0200

    Properly implemented transactions in ingestion command.

--- ngeo_browse_server/ngeo_browse_server/control/management/commands/ngeo_ingest_browse_report.py
+++ ngeo_browse_server/ngeo_browse_server/control/management/commands/ngeo_ingest_browse_report.py
@@ -7,20 +7,21 @@ from django.db import transaction
 from ngeo_browse_server.control.ingest import ingest_browse_report
 
 
-
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option('--on-error',
             dest='on_error', default="rollback",
             choices=["continue", "stop", "rollback"],
             help="Declare how errors shall be handled. Default is 'rollback'."
-        )
+        ),
     )
     
-    args = '< browse-report-xml-file browse-report-xml-file ... >'
+    args = 'browse-report-xml-file1 [browse-report-xml-file2] ... >'
     help = 'Ingests the specified ngEO Browse Reports.'
 
-    def handle(self, on_error, *filenames):
+    def handle(self, *filenames, **kwargs):
+        if not len(filenames):
+            raise CommandError("No input files given.")
         with transaction.commit_manually():
             for filename in filenames:
                 sid = transaction.savepoint()
@@ -28,13 +29,18 @@ class Command(BaseCommand):
                     document = etree.parse(filename)
                     ingest_browse_report(document)
                 except Exception, e:
+                    on_error = kwargs["on_error"]
                     if on_error == "continue":
                         # TODO: write warning
                         transaction.savepoint_rollback(sid)
                     elif on_error == "stop":
                         transaction.savepoint_rollback(sid)
                         transaction.commit()
-                        raise CommandError(str(e))
+                        raise
                     elif on_error == "rollback":
                         transaction.rollback()
-                        raise CommandError(str(e))
+                        raise
+                
+                transaction.savepoint_commit(sid)
+            
+            transaction.commit()
