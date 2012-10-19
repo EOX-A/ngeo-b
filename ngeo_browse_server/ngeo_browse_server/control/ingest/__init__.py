7a8e007b0e5b3356a011941729cf90ebfbf35b53
Fabian Schindler <fabian.schindler@gmx.at>
2012-10-19 16:32:10 +0200

    Refactored ingestion in order to correctly support ingestion results. General improvements on structure and interfaces.

--- ngeo_browse_server/ngeo_browse_server/control/ingest/__init__.py
+++ ngeo_browse_server/ngeo_browse_server/control/ingest/__init__.py
@@ -1,3 +1,4 @@
+import sys
 from os.path import isabs, isdir, join
 from itertools import product
 from numpy import arange
@@ -14,30 +15,132 @@ from ngeo_browse_server.config import get_ngeo_config
 from ngeo_browse_server.control.ingest.parsing import (
     parse_browse_report, parse_coord_list
 )
-from ngeo_browse_server.config.models import (
-    RectifiedBrowse, FootprintBrowse, RegularGridBrowse
-)
+from ngeo_browse_server.control.ingest import data
+from ngeo_browse_server.config import models
 
 
-def ingest_browse_report(document):
-    """ Ingests a browse report.
+def _model_from_parsed(parsed_browse, browse_report, model_cls):
+    return model_cls.objects.create(browse_report=browse_report,
+                                    **parsed_browse.get_kwargs())
+
+
+def ingest_browse_report(parsed_browse_report, path_prefix, 
+                         reraise_exceptions=False):
+    """ Ingests a browse report. reraise_exceptions if errors shall be handled 
+    externally
     """
     
     # initialize the EOxServer system/registry/configuration
     System.init()
     
-    # parse the browse report and create the models
-    browse_report, browses = parse_browse_report(document.getroot())
-    
     config = get_ngeo_config()
     opt_dir = config.get("control.ingest", "optimized_files_dir")
+    
+    
     if not isabs(opt_dir):
         opt_dir = join(settings.PROJECT_DIR, opt_dir)
     
-    # Initialize the format selection and the preprocessor to create 
-    # optimized datasets
+    
     format_selection = get_format_selection("GTiff") # TODO: use more options
     preprocessor = WMSPreProcessor(format_selection, bandmode=RGB) # TODO: use options
+    
+    browse_type = models.BrowseType.objects.get_or_create(id=parse_browse_report.browse_type)
+    browse_report = models.BrowseReport.objects.create(browse_type=browse_type,
+                                                       **parsed_browse_report.get_kwargs())
+    
+    result = IngestResult()
+    
+    for parsed_browse in parsed_browse_report:
+        try:
+            replaced = ingest_browse(parsed_browse, browse_report, preprocessor,
+                                     opt_dir)
+            result.add(parsed_browse.browse_identifier, replaced)
+        except Exception, e:
+            if reraise_exceptions:
+                raise sys.exc_info()
+            else:
+                # TODO: use transaction savepoints to keep the DB in a 
+                # consistent state
+                result.add_failure(parsed_browse.browse_identifier, 
+                                   type(e).__name__, str(e))
+        
+    return result
+    
+
+def ingest_browse(parsed_browse, browse_report, preprocessor, opt_dir):
+    replaced = False
+    output_filename = join(opt_dir, parsed_browse.file_name)
+        
+    srid = fromShortCode(parsed_browse.reference_system_identifier)
+    swap_axes = hasSwappedAxes(srid)
+    
+    # TODO: maybe we need to wrap this in a transaction savepoint
+    
+    # check if a browse already exists and delete it in order to replace it
+    try:
+        browse = models.Browse.objects.get(browse_identifier__id=parsed_browse.browse_identifier)
+        browse.delete()
+        replaced = True
+    except models.Browse.DoesNotExist:
+        pass
+    
+    
+    # initialize a GeoReference for the preprocessor
+    geo_reference = None
+    if type(parsed_browse) is data.RectifiedBrowse:
+        geo_reference = Extent(parsed_browse.minx, parsed_browse.miny, 
+                               parsed_browse.maxx, parsed_browse.maxy,
+                               srid)
+        model = _model_from_parsed(parsed_browse, browse_report,
+                                   models.RectifiedBrowse)
+        
+    elif type(parsed_browse) is data.FootprintBrowse:
+        pixels = parse_coord_list(parsed_browse.col_row_list)
+        coords = parse_coord_list(parsed_browse.coord_list, swap_axes)
+        geo_reference = GCPList(zip(pixels, coords), srid)
+        
+        model = _model_from_parsed(parsed_browse, browse_report,
+                                   models.FootprintBrowse)
+        
+    elif type(parsed_browse) is data.RegularGridBrowse:
+        range_x = arange(0.0, parsed_browse.col_node_number * parsed_browse.col_step, parsed_browse.col_step)
+        range_y = arange(0.0, parsed_browse.row_node_number * parsed_browse.row_step, parsed_browse.row_step)
+        
+        # Python is cool!
+        pixels = [(x, y) for y in range_y for x in range_x]
+        
+        # get the coordinates as tuple-lists
+        coords = []
+        for coord_list in parsed_browse.coord_lists:
+            coords.extend(parse_coord_list(coord_list, swap_axes))
+        
+        gcps = [(x, y, pixel, line) 
+                for (x, y), (pixel, line) in zip(coords, pixels)]
+        geo_reference = GCPList(gcps, srid)
+        
+        model = _model_from_parsed(parsed_browse, browse_report,
+                                   models.RegularGridBrowse)
+        
+        for coord_list in parsed_browse.coord_lists:
+            models.RegularGridCoordList.objects.create(regular_grid_browse=model,
+                                                       coord_list=coord_list)
+    
+    else:
+        raise NotImplementedError
+    
+    if parsed_browse.browse_identifier is not None:
+        models.BrowseIdentifier.objects.create(id=parsed_browse.browse_identifier, 
+                                               browse=model) 
+
+    # start the preprocessor
+    result = preprocessor.process(parsed_browse.file_name, output_filename,
+                                  geo_reference, generate_metadata=True)
+    
+    # create EO metadata necessary for registration
+    eo_metadata = EOMetadata(
+        parsed_browse.browse_identifier, parsed_browse.start_time, 
+        parsed_browse.end_time, result.footprint_geom
+    )
     
     # initialize the Coverage Manager for Rectified Datasets to register the
     # datasets in the database
@@ -48,68 +151,51 @@ def ingest_browse_report(document):
         }
     )
     
+    # register the optimized dataset
+    rect_mgr.create(obj_id=parsed_browse.browse_identifier.id, 
+                    range_type_name="RGB", default_srid=srid, visible=False,
+                    local_path=result.output_filename,
+                    eo_metadata=eo_metadata, force=False)
     
-    for browse in browses:
-        # create an output filename to store the optimized file
-        output_filename = join(opt_dir, browse.file_name)
+    return replaced
+
+
+class IngestResult(object):
+    """ Result object for ingestion operations. """
+    
+    def __init__(self):
+        self._inserted = 0
+        self._replaced = 0
+        self._records = []
+        self._message = "" # TODO: automatically generate error message?
+    
+    
+    def add(self, identifier, replaced=False, status="success"):
+        if replaced:
+            self._replaced += 1
+        else:
+            self._inserted += 1
         
-        # get the srid of the browse and check if the coordinate axes need to 
-        # be swapped 
-        srid = fromShortCode(browse.reference_system_identifier)
-        swap_axes = hasSwappedAxes(srid)
+        assert(status in ("success", "partial"))
         
-        # initialize a GeoReference for the preprocessor
-        geo_reference = None
-        if type(browse) is RectifiedBrowse:
-            geo_reference = Extent(browse.minx, browse.miny, 
-                                   browse.maxx, browse.maxy,
-                                   srid)
-            
-        elif type(browse) is FootprintBrowse:
-            pixels = parse_coord_list(browse.col_row_list)
-            coords = parse_coord_list(browse.coord_list, swap_axes)
-            geo_reference = GCPList(zip(pixels, coords), srid)
-            
-        elif type(browse) is RegularGridBrowse:
-            
-            range_x = arange(0.0, browse.col_node_number * browse.col_step, browse.col_step)
-            range_y = arange(0.0, browse.row_node_number * browse.row_step, browse.row_step)
-            
-            print browse.col_step, browse.col_node_number
-            print browse.row_step, browse.row_node_number
-            
-            print min(range_x), max(range_x)
-            print min(range_y), max(range_y)
-            
-            # Python is cool!
-            pixels = [(x, y) for y in range_y for x in range_x]
-            
-            coords = []
-            for coord_list in browse.coord_lists.all():
-                coords.extend(parse_coord_list(coord_list.coord_list, swap_axes))
-            
-            gcps = [(x, y, pixel, line) 
-                    for (x, y), (pixel, line) in zip(coords, pixels)]
-            geo_reference = GCPList(gcps, srid)
-            
-            print srid
+        self._records.append((identifier, status, None, None))
+    
+    
+    def add_failure(self, identifier, code, message):
+        self._records.append((identifier, "failure", code, message))
+    
+    
+    @property
+    def status(self):
+        # TODO: count errors. if errors == 0 return "success"
+        if len(filter(lambda record: record[1] == "failure", self._records)):
+            return "partial"
+        else:
+            return "success"
+    
+    to_be_replaced = property(lambda self: len(self._records)) # TODO: sure?    
+    actually_inserted = property(lambda self: self._inserted)
+    actually_replaced = property(lambda self: self._replaced)
+    
         
-        # start the preprocessor
-        result = preprocessor.process(browse.file_name, output_filename,
-                                      geo_reference, generate_metadata=True)
-        
-        # create EO metadata necessary for registration
-        eo_metadata = EOMetadata(
-            browse.browse_identifier.id, browse.start_time, browse.end_time,
-            result.footprint_geom
-        )
-        
-        # register the optimized dataset
-        rect_mgr.create(obj_id=browse.browse_identifier.id, 
-                        range_type_name="RGB", default_srid=srid, visible=False,
-                        local_path=result.output_filename,
-                        eo_metadata=eo_metadata, force=False)
-        
-        browse.save()
-        
-    # TODO: add browses to browse report and save all
+    
---
+++ ngeo_browse_server/ngeo_browse_server/control/ingest/data.py
@@ -0,0 +1,148 @@
+"""\ 
+This module contains intermediary (runtime) data for ingestion or the like.
+The classes in this module are explicitly not tied to database models, but
+provide means for easy data exchange.
+"""
+
+class Browse(object):
+    """ Abstract base class for browse records. """
+
+    def __init__(self, browse_identifier, file_name, image_type,
+                 reference_system_identifier, start_time, end_time):
+        self._browse_id = browse_identifier
+        self._file_name = file_name
+        self._image_type = image_type
+        self._reference_system_identifier = reference_system_identifier
+        self._start_time = start_time
+        self._end_time = end_time
+
+
+    browse_identifier = property(lambda self: self._browse_identifier)
+    file_name = property(lambda self: self._file_name)
+    image_type = property(lambda self: self._image_type)
+    reference_system_identifier = property(lambda self: self._reference_system_identifier)
+    start_time = property(lambda self: self._start_time)
+    end_time = property(lambda self: self._end_time)
+
+
+    def get_kwargs(self):
+        return {
+            "file_name": self.file_name,
+            "image_type": self.image_type,
+            "reference_system_identifier": self.reference_system_identifier,
+            "start_time": self.start_time,
+            "end_time": self.end_time
+        }
+
+    
+class RectifiedBrowse(Browse):
+    
+    def __init__(self, minx, miny, maxx, maxy, *args, **kwargs):
+        super(RectifiedBrowse, self).__init__(*args, **kwargs)
+        self._extent = minx, miny, maxx, maxy
+    
+    minx = property(lambda self: self._extent[0])
+    miny = property(lambda self: self._extent[1])
+    maxx = property(lambda self: self._extent[2])
+    maxy = property(lambda self: self._extent[3])
+
+
+    def get_kwargs(self):
+        kwargs = super(RectifiedBrowse, self).get_kwargs()
+        kwargs.update({
+            "minx": self.minx,
+            "miny": self.miny,
+            "maxx": self.maxx,
+            "maxy": self.maxy
+        })
+        return kwargs
+
+class FootprintBrowse(Browse):
+    
+    def __init__(self, node_number, col_row_list, coord_list, *args, **kwargs):
+        super(FootprintBrowse, self).__init__(*args, **kwargs)
+        self._node_number = node_number
+        self._col_row_list = col_row_list
+        self._coord_list = coord_list
+
+
+    node_number = property(lambda self: self._node_number)
+    col_row_list = property(lambda self: self._col_row_list)
+    coord_list = property(lambda self: self._coord_list)
+    
+    
+    def get_kwargs(self):
+        kwargs = super(FootprintBrowse, self).get_kwargs()
+        kwargs.update({
+            "node_number": self._node_number,
+            "col_row_list": self._col_row_list,
+            "coord_list": self._coord_list
+        })
+        return kwargs
+
+
+class RegularGridBrowse(Browse):
+    
+    def __init__(self, col_node_number, row_node_number, col_step, row_step, 
+                 coord_lists, *args, **kwargs):
+        
+        super(RegularGridBrowse, self).__init__(*args, **kwargs)
+        
+        self._col_node_number = col_node_number
+        self._row_node_number = row_node_number
+        self._col_step = col_step
+        self._row_step = row_step
+        self._coord_lists = coord_lists
+
+    col_node_number = property(lambda self: self._col_node_number)
+    row_node_number = property(lambda self: self._row_node_number)
+    col_step = property(lambda self: self._col_step)
+    row_step = property(lambda self: self._row_step)
+    coord_lists = property(lambda self: self._coord_lists)
+    
+    def get_kwargs(self):
+        kwargs = super(RegularGridBrowse, self).get_kwargs()
+        kwargs.update({
+            "col_node_number": self._col_node_number,
+            "row_node_number": self._row_node_number,
+            "col_step": self._col_step,
+            "row_step": self._row_step
+        })
+        return kwargs
+
+    
+class VerticalCurtainBrowse(Browse):
+    pass
+
+
+class ModelInGeotiffBrowse(Browse):
+    pass
+
+
+class BrowseReport(object):
+    """ Browse report data model. """
+    
+    def __init__(self, browse_type, date_time, responsible_org_name, browses):
+        self._browse_type = browse_type
+        self._date_time = date_time
+        self._responsible_org_name = responsible_org_name
+        self._browses = list(browses)
+    
+    
+    def __iter__(self):
+        return iter(self._browses)
+    
+    
+    def append(self, browse):
+        self._browses.append(browse)
+    
+    
+    browse_type = property(lambda self: self._browse_type)
+    date_time = property(lambda self: self._date_time)
+    responsible_org_name = property(lambda self: self._responsible_org_name)
+    
+    def get_kwargs(self):
+        return {
+            "date_time": self._date_time,
+            "responsible_org_name": self._responsible_org_name
+        }
