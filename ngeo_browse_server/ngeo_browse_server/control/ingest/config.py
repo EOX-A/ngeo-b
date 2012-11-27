77a1901a9155487f856580c5527b37a734a771f8
Fabian Schindler <fabian.schindler@gmx.at>
2012-11-27 11:59:48 +0100

    Small refactoring. Changed `get_optimized_path` to accept a sub-directory name.

--- ngeo_browse_server/ngeo_browse_server/control/ingest/config.py
+++ ngeo_browse_server/ngeo_browse_server/control/ingest/config.py
@@ -6,6 +6,9 @@ from django.conf import settings
 from ngeo_browse_server.config import get_ngeo_config, safe_get
 
 
+INGEST_SECTION = "control.ingest"
+MAPCACHE_SECTION = "control.ingest.mapcache"
+
 def get_project_relative_path(path):
     """ Returns a path, relative to the defined `PROJECT_DIR` directory. """
     
@@ -21,19 +24,17 @@ def get_storage_path(file_name, storage_dir=None, config=None):
     """
     
     config = config or get_ngeo_config()
-    section = "control.ingest"
     
     if not storage_dir:
-        storage_dir = config.get(section, "storage_dir")
+        storage_dir = config.get(INGEST_SECTION, "storage_dir")
     
     return get_project_relative_path(join(storage_dir, file_name))
 
 
-def get_optimized_path(file_name, optimized_dir=None, config=None):
+def get_optimized_path(file_name, directory=None, config=None):
     """ Returns an absolute path to a filename within the storage directory for
-    optimized raster files. Uses the optimized directory if given, otherwise 
-    uses the 'control.ingest.optimized_files_dir' setting from the ngEO
-    configuration.
+    optimized raster files. Uses the 'control.ingest.optimized_files_dir' 
+    setting from the ngEO configuration.
     
     Also tries to get the postfix for optimized files from the 
     'control.ingest.optimized_files_postfix' setting from the ngEO configuration.
@@ -41,21 +42,17 @@ def get_optimized_path(file_name, optimized_dir=None, config=None):
     All relative paths are treated relative to the PROJECT_DIR directory setting.
     """
     
-    file_name = basename(file_name)
     config = config or get_ngeo_config()
     
-    section = "control.ingest"
+    file_name = basename(file_name)
+    if directory:
+        file_name = join(directory, file_name)
     
-    if not optimized_dir:
-        optimized_dir = config.get(section, "optimized_files_dir")
-        
-    optimized_dir = get_project_relative_path(optimized_dir)
+    optimized_dir = get_project_relative_path(
+        config.get(INGEST_SECTION, "optimized_files_dir")
+    )
     
-    try:
-        postfix = config.get(section, "optimized_files_postfix")
-    except NoSectionError, NoOptionError:
-        postfix = ""
-    
+    postfix = safe_get(config, INGEST_SECTION, "optimized_files_postfix", "")
     root, ext = splitext(file_name)
     return join(optimized_dir, root + postfix + ext)
 
@@ -68,20 +65,18 @@ def get_format_config(config=None):
     values = {}
     config = config or get_ngeo_config()
     
-    section = "control.ingest"
-    
-    values["compression"] = safe_get(config, section, "compression")
+    values["compression"] = safe_get(config, INGEST_SECTION, "compression")
     
     if values["compression"] == "JPEG":
-        value = safe_get(config, section, "jpeg_quality")
+        value = safe_get(config, INGEST_SECTION, "jpeg_quality")
         values["jpeg_quality"] = int(value) if value is not None else None
     
     elif values["compression"] == "DEFLATE":
-        value = safe_get(config, section, "zlevel")
+        value = safe_get(config, INGEST_SECTION, "zlevel")
         values["zlevel"] = int(value) if value is not None else None
         
     try:
-        values["tiling"] = config.getboolean(section, "tiling")
+        values["tiling"] = config.getboolean(INGEST_SECTION, "tiling")
     except: pass
     
     return values
@@ -93,18 +88,16 @@ def get_optimization_config(config=None):
     values = {}
     config = config or get_ngeo_config()
     
-    section = "control.ingest"
-    
     try:
-        values["overviews"] = config.getboolean(section, "overviews")
+        values["overviews"] = config.getboolean(INGEST_SECTION, "overviews")
     except: pass
     
     try:
-        values["color_index"] = config.getboolean(section, "color_index")
+        values["color_index"] = config.getboolean(INGEST_SECTION, "color_index")
     except: pass
     
     try:
-        values["footprint_alpha"] = config.getboolean(section, "footprint_alpha")
+        values["footprint_alpha"] = config.getboolean(INGEST_SECTION, "footprint_alpha")
     except: pass
     
     return values
@@ -116,10 +109,8 @@ def get_mapcache_config(config=None):
     values = {}
     config = config or get_ngeo_config()
     
-    section = "control.ingest.mapcache"
-    
-    values["seed_command"] = config.get(section, "seed_command")
-    values["config_file"] = config.get(section, "config_file")
-    values["threads"] = int(safe_get(config, section, "threads", 1))
+    values["seed_command"] = config.get(MAPCACHE_SECTION, "seed_command")
+    values["config_file"] = config.get(MAPCACHE_SECTION, "config_file")
+    values["threads"] = int(safe_get(config, MAPCACHE_SECTION, "threads", 1))
     
     return values
