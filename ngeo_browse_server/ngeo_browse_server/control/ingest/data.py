dfb2e509d6fd49f26c384a6a6589d197eb8c5847
Fabian Schindler <fabian.schindler@gmx.at>
2012-11-27 12:00:29 +0100

    Allowed the `len` function to work with a parsed browse report.

--- ngeo_browse_server/ngeo_browse_server/control/ingest/data.py
+++ ngeo_browse_server/ngeo_browse_server/control/ingest/data.py
@@ -132,6 +132,8 @@ class BrowseReport(object):
     def __iter__(self):
         return iter(self._browses)
     
+    def __len__(self):
+        return len(self._browses)
     
     def append(self, browse):
         self._browses.append(browse)
