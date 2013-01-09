6491b370048c6359b3fb4d6c13245632afbec8a2
Fabian Schindler <fabian.schindler@gmx.at>
2013-01-09 15:03:54 +0100

    Implemented parsing of browse layer xml structures.

--- ngeo_browse_server/ngeo_browse_server/control/browselayer/parsing.py
+++ ngeo_browse_server/ngeo_browse_server/control/browselayer/parsing.py
@@ -26,3 +26,51 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 #-------------------------------------------------------------------------------
+
+import logging
+
+from ngeo_browse_server.control.browselayer.data import BrowseLayer
+from ngeo_browse_server.control.namespace import ns_cfg
+
+
+logger = logging.getLogger(__name__)
+
+
+def parse_browse_layer(browse_layers_elem):
+    logger.info("Start parsing browse layer.")
+    
+    #TODO: browse_layers_elem.tag == ns_cfg("browseLayers")
+    
+    browse_layers = []
+    for browse_layer_elem in browse_layers_elem.findall(ns_cfg("browseLayer")):
+    
+        opt = {}
+        description_elem = browse_layer_elem.find(ns_cfg("description"))
+        if description_elem:
+            opt["description"] = description_elem.text
+        
+        rgb_bands_elem = browse_layer_elem.find(ns_cfg("rgbBands"))
+        if rgb_bands_elem:
+            r, g, b = map(int, rgb_bands_elem.text.split(","))
+            opt["r"] = r; opt["g"] = g; opt["b"] = b
+        
+        radiometric_interval_elem = browse_layer_elem.find(ns_cfg("radiometricInterval"))
+        if radiometric_interval_elem:
+            opt["min"] = int(radiometric_interval_elem.find(ns_cfg("min")).text)
+            opt["max"] = int(radiometric_interval_elem.find(ns_cfg("max")).text)
+        
+        browse_layers.append(BrowseLayer(
+            browse_layer_elem.get("browseLayerId"),
+            browse_layer_elem.fing(ns_cfg("browseType")).text,
+            browse_layer_elem.fing(ns_cfg("title")).text,
+            browse_layer_elem.fing(ns_cfg("grid")).text,
+            browse_layer_elem.fing(ns_cfg("browseAccessPolicy")).text,
+            browse_layer_elem.fing(ns_cfg("containsVerticalCurtains")).text == "true",
+            int(browse_layer_elem.fing(ns_cfg("highestMapLevel")).text),
+            int(browse_layer_elem.fing(ns_cfg("lowestMapLevel")).text),
+            **opt
+        ))
+    
+    return browse_layers
+
+    
