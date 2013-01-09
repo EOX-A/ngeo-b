8b301dc3e63408ddfda4b10c2d7ca4d6ec305a52
Fabian Schindler <fabian.schindler@gmx.at>
2013-01-09 13:26:32 +0100

    Added data class for browse layers.

--- ngeo_browse_server/ngeo_browse_server/control/browselayer/data.py
+++ ngeo_browse_server/ngeo_browse_server/control/browselayer/data.py
@@ -26,3 +26,59 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 #-------------------------------------------------------------------------------
+
+
+class BrowseLayer(object):
+    def __init__(self, browse_layer_identifier, browse_type, title, description, grid,
+                 browse_access_policy="OPEN", contains_vertical_curtains=False,
+                 r_band=None, g_band=None, b_band=None,
+                 radiometric_interval_min=None, radiometric_interval_max=None,
+                 highest_map_level=None, lowest_map_level=None):
+        self._browse_layer_identifier = browse_layer_identifier
+        self._browse_type = browse_type
+        self._title = title
+        self._description = description
+        self._grid = grid
+        self._browse_access_policy = browse_access_policy
+        self._contains_vertical_curtains = contains_vertical_curtains
+        self._r_band = r_band
+        self._g_band = g_band
+        self._b_band = b_band
+        self._radiometric_interval_min = radiometric_interval_min
+        self._radiometric_interval_max = radiometric_interval_max
+        self._highest_map_level = highest_map_level
+        self._lowest_map_level = lowest_map_level
+    
+    browse_layer_identifier = property(lambda self: self._browse_layer_identifier)
+    browse_type = property(lambda self: self._browse_type)
+    title = property(lambda self: self._title)
+    description = property(lambda self: self._description)
+    grid = property(lambda self: self._grid)
+    browse_access_policy = property(lambda self: self._browse_access_policy)
+    contains_vertical_curtains = property(lambda self: self._contains_vertical_curtains)
+    r_band = property(lambda self: self._r_band)
+    g_band = property(lambda self: self._g_band)
+    b_band = property(lambda self: self._b_band)
+    radiometric_interval_min = property(lambda self: self._radiometric_interval_min)
+    radiometric_interval_max = property(lambda self: self._radiometric_interval_max)
+    highest_map_level = property(lambda self: self._highest_map_level)
+    lowest_map_level = property(lambda self: self._lowest_map_level)
+    
+    def get_kwargs(self):
+        return {
+            "id": self.browse_layer_identifier,
+            "browse_type": self.browse_type,
+            "title": self.title,
+            "description": self.description,
+            "grid": self.grid,
+            "browse_access_policy": self.browse_access_policy,
+            "contains_vertical_curtains": self.contains_vertical_curtains,
+            "r_band": self.r_band,
+            "g_band": self.g_band,
+            "b_band": self.b_band,
+            "radiometric_interval_min": self.radiometric_interval_min,
+            "radiometric_interval_max": self.radiometric_interval_max,
+            "highest_map_level": self.highest_map_level,
+            "lowest_map_level": self.lowest_map_level
+        }
+        
