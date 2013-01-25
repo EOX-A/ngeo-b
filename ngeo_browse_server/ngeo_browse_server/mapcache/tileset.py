a8d5fc50bdd451e9028d281eda9df98b878e3bc0
Fabian Schindler <fabian.schindler@gmx.at>
2013-01-25 10:00:12 +0100

    Implemented adding tiles to a tileset sqlite (+creating sqlite DB if necessary).

--- ngeo_browse_server/ngeo_browse_server/mapcache/tileset.py
+++ ngeo_browse_server/ngeo_browse_server/mapcache/tileset.py
@@ -27,31 +27,94 @@
 # THE SOFTWARE.
 #-------------------------------------------------------------------------------
 
-from os.path import exists
+from os.path import exists, basename, isfile
 import sqlite3
-from io import BytesIO 
+from io import BytesIO
+from datetime import datetime
+
+from django.db import models, connections
+
 
 URN_TO_GRID = {
     "urn:ogc:def:wkss:OGC:1.0:GoogleMapsCompatible": "GoogleMapsCompatible",
     "urn:ogc:def:wkss:OGC:1.0:GoogleCRS84Quad": "WGS84"
 }
 
+# Django does not support multi-column primary keys
+class TileModel(models.Model):
+    tileset = models.TextField()
+    grid = models.TextField()
+    x = models.IntegerField()
+    y = models.IntegerField()
+    z = models.IntegerField()
+    data = models.TextField() # TODO: wrapper for blob
+    dim = models.TextField()
+    ctime = models.DateTimeField()
+    
+    class Meta(object):
+        db_table = "tiles"
+
+
+def open_queryset(path):
+    if not exists(path):
+        raise TileSetException("TileSet '%s' does not exist.")
+    
+    name = basename(path)
+    if not name in connections.databases:        
+        connections.databases[name] = {
+            "NAME": path,
+            "ENGINE": "django.db.backends.sqlite3"
+        }
+        
+    _ = connections[name]
+    
+    return TileModel.objects.using(name)
+
+# end TODO
 
 class TileSetException(Exception):
     pass
 
 
-def open(path):
-    if not exists(path):
+def open(path, mode="r"):
+    db_exists = isfile(path)
+    create = False
+    if not db_exists and mode == "r":
         raise TileSetException("TileSet '%s' does not exist.")
-    return TileSet(path)
+    elif not db_exists and mode == "w":
+        create = True
+    
+    # TODO: schema detection
+    # SELECT name FROM sqlite_master WHERE type = 'table';
+    # TODO: other schemas
+    return SQLiteSchemaTileSet(path, create)
 
 
-class TileSet(object):
-    def __init__(self, path):
+class SQLiteSchemaTileSet(object):
+    def __init__(self, path, create=False):
         self.path = path
+        
+        if create:
+            with sqlite3.connect(path) as connection:
+                cur = connection.cursor()
+                cur.executescript("""\
+                    create table if not exists tiles(
+                        tileset text,
+                        grid text,
+                        x integer,
+                        y integer,
+                        z integer,
+                        data blob,
+                        dim text,
+                        ctime datetime,
+                        primary key(tileset,grid,x,y,z,dim)
+                    );
+                """)
             
     def get_tiles(self, tileset, grid, dim=None, minzoom=None, maxzoom=None):
+        """ Generator function to loop over all tiles in a given zoom interval
+        and a given dimension.
+        """
         with sqlite3.connect(self.path) as connection:
             cur = connection.cursor()
             where_clauses = [
@@ -82,4 +145,15 @@ class TileSet(object):
                     break
                 
                 yield row[:-1] + (BytesIO(row[-1]),)
+    
+    def add_tile(self, tileset, grid, dim, x, y, z, f):
+        """ Add a new tile entry into the sqlite database file with the given
+        values.
+        """
+        with sqlite3.connect(self.path) as connection:
+            cur = connection.cursor()
+            cur.execute("INSERT INTO tiles VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+                        (tileset, grid, x, y, z, buffer(f.read()), dim, 
+                         datetime.now()))
+        
         
