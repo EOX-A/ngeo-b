dca751595b785ee3802264cc03df2a073fce9c28
Marko Locher <marko.locher@eox.at>
2013-04-12 14:31:53 +0200

    Fix calling manage.py test

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -4,6 +4,5 @@
 cd "$WORKSPACE/deliverables/developments/ngeo_browse_server"
 source .venv/bin/activate
 
-cd autotest
 echo "**> running tests ..."
-python manage.py test services -v2
+python manage.py test
