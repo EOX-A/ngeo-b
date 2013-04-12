8aeecaa00670080ac3dfde568231cda228d62a89
Marko Locher <marko.locher@eox.at>
2013-04-12 14:27:03 +0200

    Create venv and run tests (this commit will fail!)

--- jenkins/prepare_instance.sh
+++ jenkins/prepare_instance.sh
@@ -1,3 +1,14 @@
 #!/bin/sh -xe
 
-cd "$WORKSPACE"
+# Create the virtual environment if it does not exist
+cd "$WORKSPACE/deviverables/developments/ngeo_browse_server"
+if [ -d ".venv" ]; then
+    echo "**> virtualenv exists!"
+else
+    echo "**> creating virtualenv..."
+    virtualenv --system-site-packages .venv
+fi
+
+# activate the virtual environment
+source .venv/bin/activate
+
--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -1,2 +1,9 @@
 #!/bin/sh -xe
 
+# activate the virtual environment
+cd "$WORKSPACE/deviverables/developments/ngeo_browse_server"
+source .venv/bin/activate
+
+cd autotest
+echo "**> running tests ..."
+python manage.py test services -v2
