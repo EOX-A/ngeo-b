3b219e9831a60c6f0bb2f820e1329a917f37f3e6
Fabian Schindler <fabian.schindler@gmx.at>
2013-07-16 18:31:37 +0200

    Adjusting defualt and testing url and config setup to be inline with registration/unregistration required changes.

--- ngeo_browse_server/ngeo_browse_server/project_template/project_name/urls.py
+++ ngeo_browse_server/ngeo_browse_server/project_template/project_name/urls.py
@@ -51,5 +51,6 @@ urlpatterns = patterns('',
     # Enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
-    (r'^ingest', 'ngeo_browse_server.control.views.ingest')
+    (r'^ingest', 'ngeo_browse_server.control.views.ingest'),
+    (r'^controllerServer', 'ngeo_browse_server.control.views.controller_server'),
 )
