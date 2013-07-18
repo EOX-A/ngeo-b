f4506cfa0d5f7b27948ce1647e3c652f02a9e8df
Fabian Schindler <fabian.schindler@gmx.at>
2013-07-18 16:25:18 +0200

    Adding timeout capabilities to `FileLock`.

--- ngeo_browse_server/ngeo_browse_server/lock.py
+++ ngeo_browse_server/ngeo_browse_server/lock.py
@@ -1,5 +1,6 @@
 import os
 import errno
+import time
 
 
 class LockException(Exception):
@@ -11,30 +12,57 @@ class FileLock(object):
         synchronization. 
     """
     
-    def __init__(self, lockfile=None):
+    def __init__(self, lockfile=None, timeout=None, delay=.05):
         self.lockfile = lockfile
-        self.is_locked = False
+        self.fd = None
+
+        self.timeout = timeout
+        self.delay = delay
+    
+    @property
+    def is_locked(self):
+        """ See if we are currently locking the lock file. """
+        return self.fd is not None
 
     def acquire(self):
-        """ Acquire the lock, if possible. Raises an OSError if not. """
-        try:
-            self.fd = os.open(self.lockfile, os.O_CREAT|os.O_EXCL|os.O_RDWR)
-        except OSError as e:
-            if e.errno == errno.EEXIST:
-                raise LockException
-        self.is_locked = True
+        """ Acquire the lock, if possible. Raises a LockException if not. """
+
+        if self.is_locked:
+            return
+
+        begin = time.time()
+        while True:
+            try:
+                self.fd = os.open(self.lockfile, os.O_CREAT|os.O_EXCL|os.O_RDWR)
+                break;
+            except OSError as e:
+                if e.errno != errno.EEXIST:
+                    raise 
+                if not self.timeout or (time.time() - begin) >= self.timeout:
+                    raise LockException("File is locked.")
+                time.sleep(self.delay)
 
     def release(self):
+        """ Release the lock by deleting the lock file. """
         if self.is_locked:
             os.close(self.fd)
             os.unlink(self.lockfile)
-            self.is_locked = False
+            self.fd = None
 
     def __enter__(self):
-        # TODO: create and open lockfile with exclusive access
+        """ Context guard entry. """
         if not self.is_locked:
             self.acquire()
         return self
 
     def __exit__(self, *args):
+        """ Context guard exit. """
         self.release()
+
+    def __del__(self):
+        """ Make sure that the FileLock instance doesn't leave a lockfile
+            lying around.
+        """
+        self.release()
+
+    
