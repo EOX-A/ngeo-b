4014ee986fc7564ed736854ecb48d52c9609782c
Stephan Meissl <stephan@meissl.name>
2013-10-24 14:31:45 +0200

    Further vagrant adjustments.

--- vagrant/scripts/packages.sh
+++ vagrant/scripts/packages.sh
@@ -11,4 +11,6 @@ yum install -y libxml2 libxml2-python mapserver mapserver-python
 # Install some build dependencies
 yum install -y gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel perl wget httpd-devel pixman-devel sqlite-devel libpng-devel libjpeg-devel libcurl-devel cmake geos-devel fcgi-devel gdal-devel
 
-# Attention: Make sure to not install mapcache, ngEO_Browse_Server, and EOxServer!
+# Attention: Make sure to not install mapcache, ngEO_Browse_Server, and 
+# EOxServer from rpm packages!
+# See development_installation.sh for installation.
