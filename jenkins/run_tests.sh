3738ca093e1f301d1ddb4ea457bc768e31ecedcf
Stephan Meissl <stephan@meissl.name>
2013-10-31 18:32:22 +0100

    Adding tear down script to jenkins.

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -3,9 +3,6 @@
 # ngEO Browse Server
 NGEOB_INSTALL_DIR="$WORKSPACE"
 
-# PostgreSQL/PostGIS database
-DB_NAME="ngeo_browse_server_db"
-
 # activate the virtual environment
 cd "$NGEOB_INSTALL_DIR"
 source .venv/bin/activate
@@ -13,18 +10,3 @@ source .venv/bin/activate
 echo "**> running tests ..."
 cd ngeo_browse_server_instance
 python manage.py test control -v2
-
-
-# Tear down
-
-# Drop the DB
-if [ `psql postgres -tAc "SELECT 1 FROM pg_database WHERE datname='$DB_NAME'"` ] ; then
-    echo "Dropping ngEO Browse Server database."
-    dropdb $DB_NAME
-fi
-
-# Remove test instance
-rm -rf $NGEOB_INSTALL_DIR/ngeo_browse_server_instance/
-
-# Uninstall EOxServer
-pip uninstall --yes EOxServer
