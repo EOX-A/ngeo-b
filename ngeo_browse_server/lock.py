1b98bbc23d3a13e01de86c43ca180bddb48c02f5
Fabian Schindler <fabian.schindler@gmx.at>
2014-02-25 17:36:08 +0100

    Adding decorator for file locks.

--- ngeo_browse_server/lock.py
+++ ngeo_browse_server/lock.py
@@ -1,6 +1,34 @@
+#-------------------------------------------------------------------------------
+#
+# Project: ngEO Browse Server <http://ngeo.eox.at>
+# Authors: Fabian Schindler <fabian.schindler@eox.at>
+#
+#-------------------------------------------------------------------------------
+# Copyright (C) 2014 EOX IT Services GmbH
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+# copies of the Software, and to permit persons to whom the Software is 
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies of this Software or works derived from this Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#-------------------------------------------------------------------------------
+
 import os
 import errno
 import time
+from functools import wraps
 
 
 class LockException(Exception):
@@ -65,4 +93,15 @@ class FileLock(object):
         """
         self.release()
 
-    
+
+def file_locked(lockfile, timeout=None, delay=.05):
+    """ Decorator to secure a function with a file lock.
+    """
+
+    def outer_wrapper(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            with FileLock(lockfile, timeout, delay):
+                return func(*args, **kwargs)
+        return wrapper
+    return outer_wrapper
