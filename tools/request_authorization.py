0e7bb58ab3d888206140ce9e636e3b393566674d
Fabian Schindler <fabian.schindler@gmx.at>
2014-03-17 17:13:08 +0100

    Bug fixing in authorization script.

--- tools/request_authorization.py
+++ tools/request_authorization.py
@@ -1,8 +1,32 @@
+#-------------------------------------------------------------------------------
+#
+# Project: ngEO Browse Server <http://ngeo.eox.at>
+# Authors: Fabian Schindler <fabian.schindler@eox.at>
+#
+#-------------------------------------------------------------------------------
+# Copyright (C) 2014 EOX IT Services GmbH
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+# copies of the Software, and to permit persons to whom the Software is 
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies of this Software or works derived from this Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#-------------------------------------------------------------------------------
 
 import sys
-import urllib2
 from optparse import OptionParser
-
 from lxml import etree
 
 
@@ -12,20 +36,22 @@ def request_authorization(baseurl, user, layer, timeperiod):
     try:
         url = (
             "%s/BrwsAuthorizationCheck?UserId=%s&BrowseLayerId=%s&TimePeriod=%s" 
-            % (baseurl, user, layer, timeperiod) 
+            % (baseurl if not baseurl.endswith("/") else baseurl[:-1], 
+               user, layer, timeperiod
+            )
         )
 
-        with urllib2.urlopen(url) as f:
-            root = lxml.parse(f)
-
-        namespaces = {"ws", "http://ngeo.eo.esa.int/schema/webserver"}
-        code = root.xpath("ws:ResponseCode/text()", namespaces=namespaces)[0]
+        root = etree.parse(url).getroot()
+        code = root.findtext(
+            "{http://ngeo.eo.esa.int/schema/webserver}ResponseCode"
+        )
 
         if code == "AUTHORIZED":
             return True
 
-    except (urllib2.URLError, etree.XMLSyntaxError, IndexError):
-        # IndexError: when parsing the XML and no responseCode is present
+    except (etree.XMLSyntaxError, IOError, IndexError), e:
+        # IOError: when HTTP request failed
+        # IndexError: when parsing the XML and no ResponseCode element is present
         pass
 
     return False
@@ -42,6 +68,10 @@ if __name__ == "__main__":
     parser.add_option("-t", "--timeperiod", dest="timeperiod",
                       help="The time period.")
 
-    options, _ = parser.parse_args()
+    opts, _ = parser.parse_args()
 
-    sys.exit(0 if request_authorization(**options) else 1)
+    authorized = request_authorization(
+        opts.baseurl, opts.user, opts.layer, opts.timeperiod
+    )
+
+    sys.exit(0 if authorized else 1)
