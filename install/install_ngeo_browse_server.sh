4dbdd67d10ff003c85247d1bdb0a818dd519d154
Fabian Schindler <fabian.schindler@gmx.at>
2014-03-21 15:21:48 +0100

    Explicit execution of python.

--- install/install_ngeo_browse_server.sh
+++ install/install_ngeo_browse_server.sh
@@ -370,7 +370,7 @@ EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <mapcache>
     <auth_method name="cmdlineauth" type="cmd">
-        <template>request_authorization.py -b http://127.0.0.1:8000/webserver -u :user -l :tileset</template>
+        <template>/usr/bin/python /usr/bin/request_authorization.py -b http://127.0.0.1:8000/webserver -u :user -l :tileset</template>
         <user_header>user</user_header>
         <auth_cache type="memcache">
             <expires>1000</expires>
