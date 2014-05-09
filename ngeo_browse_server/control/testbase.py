5d6efa31705e359ed4da9557030e5baaf0c97569
Fabian Schindler <fabian.schindler@gmx.at>
2014-05-09 14:46:49 +0200

    Fixing missing default config values.

--- ngeo_browse_server/control/testbase.py
+++ ngeo_browse_server/control/testbase.py
@@ -144,9 +144,12 @@ class BaseTestCaseMixIn(object):
         (INGEST_SECTION, "delete_on_success"): "true",
         (INGEST_SECTION, "leave_original"): "false",
         (INGEST_SECTION, "strategy"): "merge",
-        (INGEST_SECTION, "merge_threshold"): "5h"
+        (INGEST_SECTION, "merge_threshold"): "5h",
         # storage_dir, success_dir, failure_dir, optimized_files_dir, and 
         # seed_command are set automatically in setUp_files.
+
+        ("mapcache", "timedimension_default"): "2014",
+        ("mapcache", "tile_query_limit_default"): "100"
     }
     
     configuration = {}
