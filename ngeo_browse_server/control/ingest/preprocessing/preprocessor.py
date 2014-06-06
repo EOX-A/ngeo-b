270962f7e32b316f358943cd1fcb5bc784320048
Stephan Meissl <stephan@meissl.name>
2014-06-06 11:22:47 +0200

    Cleanup like addint license header.

--- ngeo_browse_server/control/ingest/preprocessing/preprocessor.py
+++ ngeo_browse_server/control/ingest/preprocessing/preprocessor.py
@@ -1,3 +1,30 @@
+#-------------------------------------------------------------------------------
+#
+# Project: ngEO Browse Server <http://ngeo.eox.at>
+# Authors: Fabian Schindler <fabian.schindler@eox.at>
+#          Stephan Meissl <stephan.meissl@eox.at>
+#
+#-------------------------------------------------------------------------------
+# Copyright (C) 2013 EOX IT Services GmbH
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies of this Software or works derived from this Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#-------------------------------------------------------------------------------
 
 from django.contrib.gis.geos import (
     GEOSGeometry, MultiPolygon, Polygon, LinearRing, 
@@ -32,12 +59,9 @@ class NGEOPreProcessor(WMSPreProcessor):
         footprint_wkt = None
         
         if not geo_reference:
-            if gt == (0.0, 1.0, 0.0, 0.0, 0.0, 1.0): # TODO: maybe use a better check
+            if gt == (0.0, 1.0, 0.0, 0.0, 0.0, 1.0):
                 if ds.GetGCPCount() > 0:
-                    # apply orthorectification
-
                     geo_reference = InternalGCPs()
-
                 else:
                     raise ValueError("No geospatial reference for "
                                      "unreferenced dataset given.")
