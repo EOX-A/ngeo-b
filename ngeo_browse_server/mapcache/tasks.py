2e1b65065372dfbfeae4e353940f80fc0d18ad05
Stephan Meissl <stephan@meissl.name>
2014-06-18 18:58:57 +0200

    Adjusting the merge order in MapCache timedimension query.

--- ngeo_browse_server/mapcache/tasks.py
+++ ngeo_browse_server/mapcache/tasks.py
@@ -199,7 +199,7 @@ def add_mapcache_layer_xml(browse_layer, config=None):
             E("read-only", "true"),
             E("timedimension",
                 E("dbfile", settings.DATABASES["mapcache"]["NAME"]),
-                E("query", "select strftime('%Y-%m-%dT%H:%M:%SZ',start_time)||'/'||strftime('%Y-%m-%dT%H:%M:%SZ',end_time) from time where source_id=:tileset and (start_time<datetime(:end_timestamp,'unixepoch') and (end_time>datetime(:start_timestamp,'unixepoch')) or (start_time=end_time and start_time<=datetime(:end_timestamp,'unixepoch') and end_time>=datetime(:start_timestamp,'unixepoch'))) and maxx>=:minx and maxy>=:miny and minx<=:maxx and miny<=:maxy order by end_time desc limit " + str(browse_layer.tile_query_limit)),
+                E("query", "select strftime('%Y-%m-%dT%H:%M:%SZ',start_time)||'/'||strftime('%Y-%m-%dT%H:%M:%SZ',end_time) from time where source_id=:tileset and (start_time<datetime(:end_timestamp,'unixepoch') and (end_time>datetime(:start_timestamp,'unixepoch')) or (start_time=end_time and start_time<=datetime(:end_timestamp,'unixepoch') and end_time>=datetime(:start_timestamp,'unixepoch'))) and maxx>=:minx and maxy>=:miny and minx<=:maxx and miny<=:maxy order by end_time asc limit " + str(browse_layer.tile_query_limit)),
                 type="sqlite", default=str(browse_layer.timedimension_default)),
             *([
                 E("auth_method", "cmdlineauth")] 
