caa7062b1ccd3b9fef16f8323a1b65a0f33fb74b
Stephan Meißl <stephan@meissl.name>
2016-05-12 11:20:43 +0200

    Disabling AuthenticationMiddleware (NGEO-2056).

--- ngeo_browse_server/project_template/project_name/settings.py
+++ ngeo_browse_server/project_template/project_name/settings.py
@@ -156,7 +156,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
 # Commented because of POST requests:    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    #'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
