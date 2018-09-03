32e8888f62da5873f12582f4377363b31ff8b373
Stephan Meißl <stephan@meissl.name>
2018-09-03 15:39:58 +0200

    Adding command to delete all browses with an extent greater than 180.

---
+++ ngeo_browse_server/control/management/commands/ngeo_delete_with_big_extent.py
@@ -0,0 +1,143 @@
+#------------------------------------------------------------------------------
+#
+# Project: ngEO Browse Server <http://ngeo.eox.at>
+# Authors: Stephan Meissl <stephan.meissl@eox.at>
+#
+#------------------------------------------------------------------------------
+# Copyright (C) 2018 EOX IT Services GmbH
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies of this Software or works derived from this Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#------------------------------------------------------------------------------
+
+import logging
+from optparse import make_option
+import traceback
+from os.path import exists
+from os import remove
+
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from eoxserver.core.system import System
+
+from ngeo_browse_server.config import models
+from ngeo_browse_server.control.management.commands import LogToConsoleMixIn
+from ngeo_browse_server.control.queries import remove_browse
+
+
+logger = logging.getLogger(__name__)
+
+
+class Command(LogToConsoleMixIn, BaseCommand):
+
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '--delete', action="store_true",
+            dest='delete', default=False,
+            help=("Optional switch to delete browses with big extent.")
+        ),
+    )
+
+    help = ("Checks DB for browses with big extent.")
+
+    def handle(self, **kwargs):
+        # parse command arguments
+        self.verbosity = int(kwargs.get("verbosity", 1))
+        traceback_conf = kwargs.get("traceback", False)
+        self.set_up_logging(
+            ["ngeo_browse_server"], self.verbosity, traceback_conf
+        )
+
+        logger.info("Starting DB check for browses with big extent.")
+
+        delete = kwargs.get("delete")
+        if delete:
+            System.init()
+            logger.info("Caution, deletion enabled.")
+
+        for browse_layer_model in models.BrowseLayer.objects.all():
+            self.handle_browse_layer(browse_layer_model, delete)
+
+        logger.info("Finished DB check for browses with big extent.")
+
+    def handle_browse_layer(self, browse_layer_model, delete=False):
+
+        try:
+            logger.info("Checking layer '%s'" % browse_layer_model.id)
+
+            browses_qs = models.Browse.objects.filter(
+                browse_layer=browse_layer_model
+            ).extra(
+                tables=[
+                    '"coverages_coveragerecord" AS "cr"',
+                    '"coverages_rectifieddatasetrecord" AS "rdsr"',
+                    '"coverages_extentrecord" AS "er"'
+                ],
+                where=[
+                    "config_browse.coverage_id = cr.coverage_id",
+                    "cr.resource_ptr_id = rdsr.coveragerecord_ptr_id",
+                    "rdsr.extent_id = er.id",
+                    "(er.maxx-er.minx) > 180"
+                ]
+            )
+
+            paths_to_delete = []
+
+            logger.info("Found '%d' browse%s with big extent."
+                        % (browses_qs.count(),
+                           "s" if browses_qs.count() > 1 else ""))
+
+            if delete:
+                logger.info("Deleting browses with big extent")
+                with transaction.commit_on_success():
+                    # go through all browses to be deleted
+                    for browse_model in browses_qs:
+                        _, filename = remove_browse(
+                            browse_model, browse_layer_model,
+                            browse_model.coverage_id, []
+                        )
+
+                        paths_to_delete.append(filename)
+
+                # loop through optimized browse images and delete them
+                for file_path in paths_to_delete:
+                    if exists(file_path):
+                        remove(file_path)
+                        logger.info(
+                            "Optimized browse image deleted: %s" % file_path
+                        )
+                    else:
+                        logger.warning(
+                            "Optimized browse image to be deleted not found "
+                            "in path: %s" % file_path
+                        )
+
+            else:
+                logger.info(
+                    "Use '--delete' to delete the entries with big extent."
+                )
+
+        except Exception as e:
+            logger.error(
+                "Failure during DB check for browses with big extent."
+            )
+            logger.error(
+                "Exception was '%s': %s" % (type(e).__name__, str(e))
+            )
+            logger.debug(traceback.format_exc() + "\n")
