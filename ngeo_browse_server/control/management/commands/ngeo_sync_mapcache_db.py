91dc3b22c35f94ec401b2cc9a0d6775dff95b105
Stephan Meißl <stephan@meissl.name>
2018-12-03 23:18:32 +0100

    Improving speed of sync command.

--- ngeo_browse_server/control/management/commands/ngeo_sync_mapcache_db.py
+++ ngeo_browse_server/control/management/commands/ngeo_sync_mapcache_db.py
@@ -34,7 +34,6 @@ from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 from django.core.management import call_command
 from django.db import transaction
-from django.db.models import F, Q
 
 from eoxserver.core.system import System
 
@@ -101,6 +100,7 @@ class Command(LogToConsoleMixIn, BaseCommand):
                 source, _ = mapcache_models.Source.objects.get_or_create(
                     name=browse_layer_model.id)
 
+                logger.debug("Starting query for browses")
                 browses_qs = models.Browse.objects.filter(
                     browse_layer=browse_layer_model
                 ).extra(
@@ -127,10 +127,13 @@ class Command(LogToConsoleMixIn, BaseCommand):
                 ).values(
                     'start_time', 'end_time', 'extent'
                 ).order_by(
-                    'start_time'
+                    'start_time', 'end_time'
                 )
                 logger.info("Number browses: %s" % len(browses_qs))
 
+                logger.debug("Starting query for unique times")
+                # optimization for when there are a lot of equal time entries
+                # like for Sentinel-2
                 unique_times_qs = models.Browse.objects.filter(
                     browse_layer=browse_layer_model
                 ).values_list(
@@ -138,18 +141,27 @@ class Command(LogToConsoleMixIn, BaseCommand):
                 ).distinct(
                     'start_time', 'end_time'
                 ).order_by(
-                    'start_time'
+                    'start_time', 'end_time'
                 )
                 logger.info("Number unique times: %s" % len(unique_times_qs))
 
-                # iterate through unique times
+                logger.info("Iterating through unique times")
+                time_intervals = []
+                i = 1
                 for unique_time in unique_times_qs:
-
                     start_time = unique_time[0]
                     end_time = unique_time[1]
+
+                    logger.debug(
+                        "Working on unique time %s: %s/%s " %
+                        (i, start_time, end_time)
+                    )
+                    i += 1
+
                     minx, miny, maxx, maxy = (None,)*4
 
-                    # search for all browses with that time and combine extent
+                    # search for all browses within that time interval and
+                    # combine extent
                     time_qs = browses_qs.filter(
                         start_time=start_time,
                         end_time=end_time
@@ -191,47 +203,57 @@ class Command(LogToConsoleMixIn, BaseCommand):
                                 i for i in [maxy_tmp, maxy] if i is not None
                             )
 
-                    #TODO guess it would be faster to first compute all Time
-                    #models and save all at once?
-
-                    # search for time entries with an overlapping time span
-                    if start_time == end_time:
-                        times_qs = mapcache_models.Time.objects.filter(
-                            source=source,
-                            start_time__lte=end_time,
-                            end_time__gte=start_time
+                    # check if previous element in ordered list overlaps
+                    if (
+                        len(time_intervals) > 0 and (
+                            (
+                                (
+                                    start_time == end_time or
+                                    time_intervals[-1][0] ==
+                                    time_intervals[-1][1]
+                                ) and (
+                                    time_intervals[-1][0] <= end_time and
+                                    time_intervals[-1][1] >= start_time
+                                )
+                            ) or (
+                                time_intervals[-1][0] < end_time and
+                                time_intervals[-1][1] > start_time
+                            )
                         )
-                    else:
-                        times_qs = mapcache_models.Time.objects.filter(
-                            Q(source=source),
-                            Q(start_time__lt=end_time,
-                              end_time__gt=start_time) |
-                            Q(start_time=F("end_time"),
-                              start_time__lte=end_time,
-                              end_time__gte=start_time)
-                        )
-
-                    if len(times_qs) > 0:
-                        # If there are overlapping time entries, merge them
-                        for time_model in times_qs:
-                            minx = min(minx, time_model.minx)
-                            miny = min(miny, time_model.miny)
-                            maxx = max(maxx, time_model.maxx)
-                            maxy = max(maxy, time_model.maxy)
-                            start_time = min(start_time, time_model.start_time)
-                            end_time = max(end_time, time_model.end_time)
-
-                        times_qs.delete()
-
-                    time_model = mapcache_models.Time(
-                        start_time=start_time, end_time=end_time, minx=minx,
-                        miny=miny, maxx=maxx, maxy=maxy, source=source
+                    ):
+                        start_time = min(start_time, time_intervals[-1][0])
+                        end_time = max(end_time, time_intervals[-1][1])
+                        minx = min(minx, time_intervals[-1][2])
+                        miny = min(miny, time_intervals[-1][3])
+                        maxx = max(maxx, time_intervals[-1][4])
+                        maxy = max(maxy, time_intervals[-1][5])
+                        time_intervals.pop(-1)
+                    time_intervals.append(
+                        (start_time, end_time, minx, miny, maxx, maxy)
                     )
 
-                    time_model.full_clean()
-                    time_model.save()
+                logger.info(
+                    "Number non-overlapping time intervals: %s" %
+                    len(time_intervals)
+                )
 
-                transaction.commit(using="mapcache")
+                logger.info(
+                    "Starting saving time intervals to MapCache SQLite file"
+                )
+                mapcache_models.Time.objects.bulk_create(
+                    [mapcache_models.Time(
+                        start_time=time_interval[0],
+                        end_time=time_interval[1],
+                        minx=time_interval[2],
+                        miny=time_interval[3],
+                        maxx=time_interval[4],
+                        maxy=time_interval[5],
+                        source=source
+                    ) for time_interval in time_intervals]
+                )
+                logger.info(
+                    "Finished saving time intervals to MapCache SQLite file"
+                )
 
             except Exception as e:
                 logger.error(
@@ -242,3 +264,6 @@ class Command(LogToConsoleMixIn, BaseCommand):
                 )
                 logger.debug(traceback.format_exc() + "\n")
                 transaction.rollback(using="mapcache")
+
+            else:
+                transaction.commit(using="mapcache")
