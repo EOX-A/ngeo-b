3cc314fb9f84a510d31675016d264b75fc114c3a
Stephan Meißl <stephan.meissl@eox.at>
2021-05-03 18:02:37 +0200

    start sxcat related services if present

--- docker/run-httpd.sh
+++ docker/run-httpd.sh
@@ -7,4 +7,11 @@ rm -rf /run/httpd/* /tmp/httpd*
 /etc/init.d/postgresql start
 /etc/init.d/memcached start
 
+if [ -f /etc/init.d/harvestd ] ; then
+    service redis start
+    service ntpd start
+    service harvestd start
+    service browsewatchd start
+fi
+
 exec /usr/sbin/apachectl -DFOREGROUND
